121441b2314d4adaf8b212353953023d13a2622d
S.Mohanapriya <smohanapriya112233@gmail.com>
2026-06-26 21:37:11 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,30 @@
+//1 ) Number of prime in a specified range 
+import java.util.Scanner
+  class prime
+  {
+    public static void main(String[] args)
+    {
+    Scanner sc=new Scanner(System.in);
+    int start=0;
+    int end=0;
+    int count=0;
+    for(int num=start;num<=end;num++)
+  {
+    factor=0;
+    for(int i=1;i<=num;i++)
+      {
+        if(num % i == 0)
+        {
+          factor++;
+        }
+      }
+    if(factor==2)
+    {
+      count++;
+    }
+  }
+System.out.println(count);
+}
+  }
+    
+    
